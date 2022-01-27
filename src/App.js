9298465869388ab9f67af66a66c762059a3f3273
Kailash <53666209+kailash230890@users.noreply.github.com>
2022-01-27 11:36:10 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ExcelFile from './ExcelFile';
+// import DataModel from './DataModel';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <DataModel /> */}
+      <ExcelFile />
+    </div>
+  );
+}
+
+export default App;
